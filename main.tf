c25e409f63703e42b5126ded606ac938116df3a9
psdixit <prashant.dixit@ice.com>
2024-06-15 00:59:11 +0530

    updated keys

--- main.tf
+++ main.tf
@@ -11,8 +11,8 @@ terraform {
 
 provider "aws" {
   region  = "us-west-2"
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_ACCESS_KEY_ID
+  access_key = "${env.AWS_ACCESS_KEY_ID}"
+  secret_key = "${env.AWS_SECRET_ACCESS_KEY_ID}"
 }
 
 resource "aws_instance" "app_server" {
