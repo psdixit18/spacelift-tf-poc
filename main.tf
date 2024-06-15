94401b830f750872ec3b0a4d8c5957d5761b73f6
psdixit <prashant.dixit@ice.com>
2024-06-15 13:24:43 +0530

    creds update

--- main.tf
+++ main.tf
@@ -14,7 +14,10 @@ terraform {
 }
 
 provider "aws" {
-  region  = "us-west-2"  
+  region  = "us-west-2"
+  access_key = "ASIAQ55YAFBEGTQ2ILDP"
+  secret_key = "1rkATjD0h65SWPoEg9F7sQ6pv8zxsD5jKJaSQ1B4"
+  
 }
 
 resource "aws_instance" "app_server" {
